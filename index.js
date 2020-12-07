bbe1bc940cbec49654f36ff0ea62d3f4ff4a3028
Joseph Tarrazi <joseftaerrazi@gmail.com>
2020-12-07 10:00:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //let sourceArray=[1, 2, 3, -9];
 
 function mapToNegativize(sourceArray){
-  for(let elem of sourceArray){
+  for(let i=0; i<sourceArray.length;i++){
   sourceArray.map(sourceArray[i]*-1);}
 }
 mapToNegativize([1, 2, 3, -9]);
